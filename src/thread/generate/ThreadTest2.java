4841e2c9df4331a7a050ce7d1568cbff23ead077
LacteaMariposa <s2218@e-mirim.hs.kr>
2023-01-03 14:36:34 +0900

    thread 연습

--- src/thread/generate/ThreadTest2.java
+++ src/thread/generate/ThreadTest2.java
@@ -1,16 +1,25 @@
 package thread.generate;
 
+import java.awt.desktop.ScreenSleepEvent;
+
 public class ThreadTest2 implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		for(int i = 0; i<100; i++){
+			System.out.println("thread 실행"+(i+1));
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ThreadTest2 t2 = new ThreadTest2();
+		Thread t3 = new Thread(t2);
+		t3.start();
 	}
 
 }
