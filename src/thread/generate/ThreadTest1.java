762ee8e73f9568640c55c0b36ffcc8dc2601c34a
LacteaMariposa <s2218@e-mirim.hs.kr>
2023-01-03 15:04:55 +0900

    init commit

--- src/thread/generate/ThreadTest1.java
+++ src/thread/generate/ThreadTest1.java
@@ -4,10 +4,14 @@ import java.util.Iterator;
 
 public class ThreadTest1 extends Thread {
 	
+	public ThreadTest1(String name) {
+		super(name);//thread 이름 설정
+	}
+	
 	@Override
 	public void run() {
 		for(int i = 0; i<100; i++){
-			System.out.println("thread 실행"+(i+1));
+			System.out.println(getName()+"thread 실행"+(i+1));
 			try {
 				sleep(500);
 			} catch (InterruptedException e) {
@@ -17,8 +21,11 @@ public class ThreadTest1 extends Thread {
 	}
 	
 	public static void main(String[] args) {
-		ThreadTest1 t1 = new ThreadTest1();
+		ThreadTest1 t1 = new ThreadTest1("첫번째 ");
 		t1.start();
+		
+		ThreadTest1 t2 = new ThreadTest1("두번째 ");
+		t2.start();
 	}
 
 }
